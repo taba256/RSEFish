c3cebacfd0fc6fc8d7f9f8916b70e0e0c901e9ad
taba <taba256@users.noreply.github.com>
2021-02-12 20:25:47 +0900

    仮完成

---
+++ main.js
@@ -0,0 +1,143 @@
+import * as UI from "./modules/ui.js";
+import * as Poke from "./modules/pokemon.js";
+
+const RS_ID_SEARCH_MAX = 50000;
+const RS_TREND_SEARCH_MAX = 10;
+const E_TREND_SEARCH_MAX = 1000;
+let searching=false;
+
+async function startSearchCB(){
+	if(searching){
+		return;
+	}
+	searching=true;
+	const gameVersion=UI.getGameVersion();
+	const searchMethod=UI.getSearchMethod();
+	const [trend1,trend2]=UI.getTrends();
+	if(gameVersion=="RS"){
+		UI.updateResultTableHead(["←","ID消費f","TID","SID","流行語待機f","流行語1","流行語2","釣り乱数","IDくじ"]);
+		UI.initResultTableBody();
+		const tid=UI.getTID();
+		const seed=UI.getSeed();
+		const rand=new Poke.GPRNG(seed);
+		if(searchMethod=="fromTIDTrend"){
+			for(let i=0;i<RS_ID_SEARCH_MAX;i++){	//乱数シード値初期化からRS_ID_SEARCH_MAX個の乱数を探索する
+				const sid_temp=rand.next();	//裏ID生成
+				const rand_tmp=new Poke.GPRNG(rand.seed);
+				const tid_temp=rand_tmp.next();
+				if(tid==tid_temp){	//表IDが指定したものと一致するなら流行語生成チェックに入る
+					const results=await Poke.searchByTrends(rand_tmp.seed,trend1,trend2,RS_TREND_SEARCH_MAX);	//裏ID決定からRS_TREND_SEARCH_MAX個以内の乱数のうち、指定した流行語になる乱数を探す
+					results.forEach(result=>{
+						UI.updateResultTableBody([null,i,tid,sid_temp,result[0],Poke.getTrendWordById(result[1].words[0]),Poke.getTrendWordById(result[1].words[1]),result[1].fishSeed.toString(16).padStart(4,"0"),result[2]],
+							()=>{
+								const points=Poke.getFishingPointByTrend(result[1]);
+								UI.plotFishingPoint(points);
+							});
+					});
+				}
+			}
+		}else if(searchMethod=="fromTID"){
+			for(let i=0;i<RS_ID_SEARCH_MAX;i++){	//乱数シード値初期化からRS_ID_SEARCH_MAX個の乱数を探索する
+				const sid_temp=rand.next();	//裏ID生成
+				const rand_tmp=new Poke.GPRNG(rand.seed);
+				const tid_temp=rand_tmp.next();
+				if(tid==tid_temp){	//表IDが指定したものと一致するなら流行語生成チェックに入る
+					const results=(await Poke.searchSimple(rand_tmp.seed,3,4)).filter(result=>result[0]==3);	//裏ID決定から5個以内の乱数のうち、2～4
+					results.forEach(result=>{
+						UI.updateResultTableBody([null,i,tid,sid_temp,result[0],Poke.getTrendWordById(result[1].words[0]),Poke.getTrendWordById(result[1].words[1]),result[1].fishSeed.toString(16).padStart(4,"0"),result[2]],
+							()=>{
+								const points=Poke.getFishingPointByTrend(result[1]);
+								UI.plotFishingPoint(points);
+							});
+					});
+				}
+			}
+		}else if(searchMethod=="fromPoint"){
+			const searchPoint=UI.getSearchPoint();
+			let seeds=new Array(0x10000);
+			for(let seed=0;seed<0x10000;seed++){
+				seeds[seed]=false;
+				const points=new Poke.FishRNG(seed).getFishPosIndex();
+				if(points.includes(searchPoint)){
+					seeds[seed]=true;
+				}
+			}
+			for(let i=0;i<RS_ID_SEARCH_MAX;i++){	//乱数シード値初期化からRS_ID_SEARCH_MAX個の乱数を探索する
+				const sid_temp=rand.next();	//裏ID生成
+				const rand_tmp=new Poke.GPRNG(rand.seed);
+				const tid_temp=rand_tmp.next();
+				const result=(await Poke.searchSimple(rand_tmp.seed,3,4)).filter(result=>result[0]==3)[0];
+				if(seeds[result[1].fishSeed]){
+					UI.updateResultTableBody([null,i,tid_temp,sid_temp,result[0],Poke.getTrendWordById(result[1].words[0]),Poke.getTrendWordById(result[1].words[1]),result[1].fishSeed.toString(16).padStart(4,"0"),result[2]],
+						()=>{
+							const points=Poke.getFishingPointByTrend(result[1]);
+							UI.plotFishingPoint(points);
+						});
+				}
+			}
+		}
+	}else if(gameVersion=="E"){
+		UI.updateResultTableHead(["←","TID(seed)","推定SID","流行語待機f","流行語1","流行語2","釣り乱数","IDくじ"]);
+		UI.initResultTableBody();
+		const tidseed=UI.getTID();	//表IDがそのまま初期シードになる
+		if(searchMethod=="fromTIDTrend"){
+			const results=await Poke.searchByTrends(tidseed,trend1,trend2,E_TREND_SEARCH_MAX);	//表ID決定からE_TREND_SEARCH_MAX個以内の乱数のうち、指定した流行語になる乱数を探す
+			results.forEach(result=>{
+				const sidrand=new Poke.GPRNG(tidseed);
+				const cnt=result[0]-4;//仮。裏ID生成の1個+初期化処理3フレームで4としているが、初期化時間にばらつきがあるかもしれないので要注意
+				for(let i=0;i<cnt;i++){
+					sidrand.next();
+				}
+				UI.updateResultTableBody([null,tidseed,sidrand.next(),result[0],Poke.getTrendWordById(result[1].words[0]),Poke.getTrendWordById(result[1].words[1]),result[1].fishSeed.toString(16).padStart(4,"0"),result[2]],
+					()=>{
+						const points=Poke.getFishingPointByTrend(result[1]);
+						UI.plotFishingPoint(points);
+					});
+			});
+		}else if(searchMethod=="fromTID"){
+			const results=await Poke.searchSimple(tidseed,500,E_TREND_SEARCH_MAX);
+			results.forEach(result=>{
+				const sidrand=new Poke.GPRNG(tidseed);
+				const cnt=result[0]-4;//仮。裏ID生成の1個+初期化処理3フレームで4としているが、初期化時間にばらつきがあるかもしれないので要注意
+				for(let i=0;i<cnt;i++){
+					sidrand.next();
+				}
+				UI.updateResultTableBody([null,tidseed,sidrand.next(),result[0],Poke.getTrendWordById(result[1].words[0]),Poke.getTrendWordById(result[1].words[1]),result[1].fishSeed.toString(16).padStart(4,"0"),result[2]],
+					()=>{
+						const points=Poke.getFishingPointByTrend(result[1]);
+						UI.plotFishingPoint(points);
+					});
+			});
+		}else if(searchMethod=="fromPoint"){
+			const searchPoint=UI.getSearchPoint();
+			let seeds=new Array(0x10000);
+			for(let seed=0;seed<0x10000;seed++){
+				seeds[seed]=false;
+				const points=new Poke.FishRNG(seed).getFishPosIndex();
+				if(points.includes(searchPoint)){
+					seeds[seed]=true;
+				}
+			}
+			const results=await Poke.searchSimple(tidseed,500,E_TREND_SEARCH_MAX);
+			results.forEach(result=>{
+				if(seeds[result[1].fishSeed]){
+					const sidrand=new Poke.GPRNG(tidseed);
+					const cnt=result[0]-4;//仮。裏ID生成の1個+初期化処理3フレームで4としているが、初期化時間にばらつきがあるかもしれないので要注意
+					for(let i=0;i<cnt;i++){
+						sidrand.next();
+					}
+					UI.updateResultTableBody([null,tidseed,sidrand.next(),result[0],Poke.getTrendWordById(result[1].words[0]),Poke.getTrendWordById(result[1].words[1]),result[1].fishSeed.toString(16).padStart(4,"0"),result[2]],
+						()=>{
+							const points=Poke.getFishingPointByTrend(result[1]);
+							UI.plotFishingPoint(points);
+						});
+				}
+			});
+		}
+	}
+	searching=false;
+}
+
+window.addEventListener("DOMContentLoaded",()=>{
+	UI.UIInit(startSearchCB);
+});
---
+++ modules/ui.js
@@ -0,0 +1,88 @@
+
+const UIInit=(startSearchCB)=>{
+	const buttonStartSearch=document.getElementById("startSearch");
+	buttonStartSearch.addEventListener("click",startSearchCB);
+};
+
+const getGameVersion=()=>{
+	return document.searchForm.GameVersion.value;
+};
+
+const getSearchMethod=()=>{
+	return document.searchForm.SearchMethod.value;
+};
+const getSearchPoint=()=>{
+	return parseInt(document.searchForm.SearchPoint.value);
+};
+
+const getTID=()=>{
+	const tid=document.searchForm.TID;
+	if(tid.validity.valid){
+		return parseInt(tid.value,10);
+	}
+	return null;
+};
+
+const getSeed=()=>{
+	const seed=document.searchForm.InitialSeed;
+	if(seed.validity.valid){
+		return parseInt(seed.value,16);
+	}
+	return null;
+};
+
+const getTrends=()=>{
+	const selectTrendWord1=document.searchForm.TrendWord1;
+	const selectTrendWord2=document.searchForm.TrendWord2;
+	return [parseInt(selectTrendWord1.value,10),parseInt(selectTrendWord2.value,10)];
+};
+
+const updateResultTableHead=(headers)=>{
+	const head=document.getElementById("resultTableHead");
+	while(head.children.length>0){
+		head.removeChild(head.children[0]);
+	}
+	const tr=document.createElement("tr");
+	headers.forEach(header=>{
+		const th=document.createElement("th");
+		th.textContent=header;
+		tr.appendChild(th);
+	});
+	head.appendChild(tr);
+}
+
+const initResultTableBody=()=>{
+	const body=document.getElementById("resultTableBody");
+	while(body.children.length>0){
+		body.removeChild(body.children[0]);
+	}
+};
+const updateResultTableBody=(results,callback)=>{
+	const body=document.getElementById("resultTableBody");
+	const tr=document.createElement("tr");
+	results.forEach((result,index)=>{
+		const td=document.createElement("td");
+		if(index==0){
+			const radio=document.createElement("input");
+			radio.type="radio";
+			radio.name="resultRadio";
+			radio.addEventListener("change",callback);
+			td.appendChild(radio);
+		}else{
+			td.textContent=result;
+		}
+		tr.appendChild(td);
+	});
+	body.appendChild(tr);
+}
+
+const plotFishingPoint=(points)=>{
+	points.forEach((point,index)=>{
+		const id="fish-"+(index+1);
+		const circlediv=document.getElementById(id);
+		circlediv.style.left=point[0]*16+"px";
+		circlediv.style.top=point[1]*16+"px";
+	});
+};
+
+export {UIInit,getGameVersion,getSearchMethod,getSearchPoint,getTID,getSeed,getTrends,updateResultTableHead,initResultTableBody,updateResultTableBody,plotFishingPoint};
